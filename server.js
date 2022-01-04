83c8fc4353daab654c3327339d0cc9e87fb37cc3
Loic <dark.evengyl@gmail.com>
2022-01-04 16:21:18 +0100

    add json + IMP res.end() impletation

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const http = require("http")
 const url = require("url")
+const fs = require("fs")
 
 
 const server = http.createServer((req, res) => {
 
     let urlParse = url.parse(req.url, true)
-    console.log(req.method)
 
     let contentRes = ""
     let statusCode = 500
+    let head = { "Content-Type" : "text/html; charset=utf-8" }
 
     if(req.method == "GET")
     {
@@ -17,6 +18,10 @@ const server = http.createServer((req, res) => {
             statusCode = 200
             contentRes = `<h1>Page d'accueil</h1>
             <a href='/categs'>Vers les catégories principales</a>`
+
+            res.writeHead(statusCode, head)
+            res.write(contentRes)
+            res.end()
         }
         else if(urlParse.pathname == "/contact")
         {
@@ -25,6 +30,10 @@ const server = http.createServer((req, res) => {
                 <input type="text" name="name"><br>
                 <input type="text" name="lastname">
                 <button type="submit">Envoyer</button>`
+
+            res.writeHead(statusCode, head)
+            res.write(contentRes)
+            res.end()
         }
         else if(urlParse.pathname.includes("/categs")) 
         {
@@ -41,6 +50,10 @@ const server = http.createServer((req, res) => {
                         <p>Categ principale : ${urlParse.query.categID}</p>
                         <p>Categ secondaire : ${urlParse.query.subcategID}</p>
                         <p>Produit sélectionné : ${urlParse.query.productID}<p>`
+
+                        res.writeHead(statusCode, head)
+                        res.write(contentRes)
+                        res.end()
                     }
                     else //localhost:3000/categs/subcategs/products?categID=1&subcategID=2
                     {
@@ -55,6 +68,10 @@ const server = http.createServer((req, res) => {
                             <li><a href="/categs/subcategs/products?categID=${urlParse.query.categID}&subcategID=${urlParse.query.subcategID}&productID=4">Product 4</a></li>
                         </ul>
                         `
+
+                        res.writeHead(statusCode, head)
+                        res.write(contentRes)
+                        res.end()
                     }
                     
                 }
@@ -70,25 +87,44 @@ const server = http.createServer((req, res) => {
                         <li><a href="/categs/subcategs/products?categID=${urlParse.query.categID}&subcategID=4">Subcateg 4</a></li>
                     </ul>
                     `
+
+                    res.writeHead(statusCode, head)
+                    res.write(contentRes)
+                    res.end()
                 }
             }
             else //localhost:3000/categs
             {
                 statusCode = 200
-                contentRes = `<h1>Vous êtes sur les catégories principales</h1>
-                <ul>
-                    <li><a href="/categs/subcategs?categID=1">Categ principale 1</a></li>
-                    <li><a href="/categs/subcategs?categID=2">Categ principale 2</a></li>
-                    <li><a href="/categs/subcategs?categID=3">Categ principale 3</a></li>
-                    <li><a href="/categs/subcategs?categID=4">Categ principale 4</a></li>
-                </ul>`
-                
+
+                fs.readFile("./content/categs.json", (error, datas) => {
+                    console.log(error)
+
+                    datas = JSON.parse(datas.toString())
+
+                    contentRes = `<h1>Vous êtes sur les catégories principales</h1>
+                    <ul>`
+
+                    datas.forEach(item => {
+                        contentRes += `<li><a href="/categs/subcategs?categID=${item.id}">${item.name}</a></li>`
+                    })
+
+                    contentRes += `</ul>`
+
+                    res.writeHead(statusCode, head)
+                    res.write(contentRes)
+                    res.end()
+                })
             }
         }
         else
         {
             statusCode = 404
             contentRes = "<h1>Oops, page non trouvée</h1>"
+
+            res.writeHead(statusCode, head)
+            res.write(contentRes)
+            res.end()
         }
 
     }
@@ -126,6 +162,10 @@ const server = http.createServer((req, res) => {
             statusCode = 303
             head = { "Location" : "/"}
 
+            res.writeHead(statusCode, head)
+            res.write(contentRes)
+            res.end()
+
 
             // statusCode 302 -> redirection standard
             // 307 -> passer de get à post puis redirger vers le meme lien en post 
@@ -136,15 +176,11 @@ const server = http.createServer((req, res) => {
     {
         statusCode = 404
         contentRes = `<h1>Je ne connais pas cette méthode HTTP : ${req.method}</h1>`
+
+        res.writeHead(statusCode, head)
+        res.write(contentRes)
+        res.end()
     }
-
-
-
-    //localhost:3000/categ/subcateg/product?categID=1&subcategID=2&productID=3
-
-    res.writeHead(statusCode, { "Content-Type" : "text/html; charset=utf-8" })
-    res.write(contentRes)
-    res.end()
 })
 
 server.listen(3000)
