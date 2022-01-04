dfe643f3f681003b5dfa170c29bf76c5b90bed01
Loic <dark.evengyl@gmail.com>
2022-01-04 09:54:50 +0100

    explain simple get post redirect

--- server.js
+++ server.js
@@ -9,108 +9,38 @@ const server = http.createServer((req, res) => {
 
     let contentRes = ""
     let statusCode = 500
+    let head = { "Content-Type" : "text/html; charset=utf-8" }
 
     if(req.method == "GET")
     {
-        if(urlParse.pathname == "/" || urlParse.pathname == "/accueil")
-        {
-            statusCode = 200
-            contentRes = `<h1>Page d'accueil</h1>
-            <a href='/categs'>Vers les catégories principales</a>`
-        }
-        else if(urlParse.pathname == "/contact")
+        if(urlParse.pathname == "/contact")
         {
             statusCode = 200
             contentRes = `<h1>Page de contact en mode GET</h1>
-                <input type="text" name="name"><br>
-                <input type="text" name="lastname">
-                <button type="submit">Envoyer</button>`
+                <form method="post">
+                    <input type="text" name="name"><br>
+                    <input type="text" name="lastname">
+                    <button type="submit">Envoyer</button>
+                </form>`
         }
-        else if(urlParse.pathname.includes("/categs")) 
+        else if(urlParse.pathname == "/")
         {
-
-            if(urlParse.pathname.includes("/subcategs")) 
-            {
-
-                if(urlParse.pathname.includes("/products"))
-                {
-                    if(urlParse.query.productID)//localhost:3000/categs/subcategs/products?categID=1&subcategID=2&productID=42
-                    {
-                        statusCode = 200
-                        contentRes = `<h1>Vous êtes sur un produit</h1>
-                        <p>Categ principale : ${urlParse.query.categID}</p>
-                        <p>Categ secondaire : ${urlParse.query.subcategID}</p>
-                        <p>Produit sélectionné : ${urlParse.query.productID}<p>`
-                    }
-                    else //localhost:3000/categs/subcategs/products?categID=1&subcategID=2
-                    {
-                        statusCode = 200
-                        contentRes = `<h1>Vous êtes sur la liste des produits</h1>
-                        <p>Categ principale : ${urlParse.query.categID}</p>
-                        <p>Categ secondaire : ${urlParse.query.subcategID}</p>
-                        <ul>
-                            <li><a href="/categs/subcategs/products?categID=${urlParse.query.categID}&subcategID=${urlParse.query.subcategID}&productID=1">Product 1</a></li>
-                            <li><a href="/categs/subcategs/products?categID=${urlParse.query.categID}&subcategID=${urlParse.query.subcategID}&productID=2">Product 2</a></li>
-                            <li><a href="/categs/subcategs/products?categID=${urlParse.query.categID}&subcategID=${urlParse.query.subcategID}&productID=3">Product 3</a></li>
-                            <li><a href="/categs/subcategs/products?categID=${urlParse.query.categID}&subcategID=${urlParse.query.subcategID}&productID=4">Product 4</a></li>
-                        </ul>
-                        `
-                    }
-                    
-                }
-                else//localhost:3000/categs/subcategs?categID=1
-                {
-                    statusCode = 200
-                    contentRes = `<h1>Vous êtes sur les catégories secondaires</h1>
-                    <p>Categ principale : ${urlParse.query.categID}</p>
-                    <ul>
-                        <li><a href="/categs/subcategs/products?categID=${urlParse.query.categID}&subcategID=1">Subcateg 1</a></li>
-                        <li><a href="/categs/subcategs/products?categID=${urlParse.query.categID}&subcategID=2">Subcateg 2</a></li>
-                        <li><a href="/categs/subcategs/products?categID=${urlParse.query.categID}&subcategID=3">Subcateg 3</a></li>
-                        <li><a href="/categs/subcategs/products?categID=${urlParse.query.categID}&subcategID=4">Subcateg 4</a></li>
-                    </ul>
-                    `
-                }
-            }
-            else //localhost:3000/categs
-            {
-                statusCode = 200
-                contentRes = `<h1>Vous êtes sur les catégories principales</h1>
-                <ul>
-                    <li><a href="/categs/subcategs?categID=1">Categ principale 1</a></li>
-                    <li><a href="/categs/subcategs?categID=2">Categ principale 2</a></li>
-                    <li><a href="/categs/subcategs?categID=3">Categ principale 3</a></li>
-                    <li><a href="/categs/subcategs?categID=4">Categ principale 4</a></li>
-                </ul>`
-                
-            }
+            statusCode = 200
+            contentRes = `<h1>Page d'accueil</h1>`
         }
-        else
-        {
-            statusCode = 404
-            contentRes = "<h1>Oops, page non trouvée</h1>"
-        }
-
     }
     else if(req.method == "POST")
     {
         if(urlParse.pathname == "/contact")
         {
-            statusCode = 200
-            contentRes = `<h1>je suis le contact en mode POST</h1>`
+            statusCode = 302
+            head = { "Location" : "/"}
         }
     }
-    else
-    {
-        statusCode = 404
-        contentRes = `<h1>Je ne connais pas cette méthode HTTP : ${req.method}</h1>`
-    }
 
 
 
-    //localhost:3000/categ/subcateg/product?categID=1&subcategID=2&productID=3
-
-    res.writeHead(statusCode, { "Content-Type" : "text/html; charset=utf-8" })
+    res.writeHead(statusCode, head)
     res.write(contentRes)
     res.end()
 })
---
+++ server_complete.js
@@ -0,0 +1,118 @@
+const http = require("http")
+const url = require("url")
+
+
+const server = http.createServer((req, res) => {
+
+    let urlParse = url.parse(req.url, true)
+    console.log(req.method)
+
+    let contentRes = ""
+    let statusCode = 500
+
+    if(req.method == "GET")
+    {
+        if(urlParse.pathname == "/" || urlParse.pathname == "/accueil")
+        {
+            statusCode = 200
+            contentRes = `<h1>Page d'accueil</h1>
+            <a href='/categs'>Vers les catégories principales</a>`
+        }
+        else if(urlParse.pathname == "/contact")
+        {
+            statusCode = 200
+            contentRes = `<h1>Page de contact en mode GET</h1>
+                <input type="text" name="name"><br>
+                <input type="text" name="lastname">
+                <button type="submit">Envoyer</button>`
+        }
+        else if(urlParse.pathname.includes("/categs")) 
+        {
+
+            if(urlParse.pathname.includes("/subcategs")) 
+            {
+
+                if(urlParse.pathname.includes("/products"))
+                {
+                    if(urlParse.query.productID)//localhost:3000/categs/subcategs/products?categID=1&subcategID=2&productID=42
+                    {
+                        statusCode = 200
+                        contentRes = `<h1>Vous êtes sur un produit</h1>
+                        <p>Categ principale : ${urlParse.query.categID}</p>
+                        <p>Categ secondaire : ${urlParse.query.subcategID}</p>
+                        <p>Produit sélectionné : ${urlParse.query.productID}<p>`
+                    }
+                    else //localhost:3000/categs/subcategs/products?categID=1&subcategID=2
+                    {
+                        statusCode = 200
+                        contentRes = `<h1>Vous êtes sur la liste des produits</h1>
+                        <p>Categ principale : ${urlParse.query.categID}</p>
+                        <p>Categ secondaire : ${urlParse.query.subcategID}</p>
+                        <ul>
+                            <li><a href="/categs/subcategs/products?categID=${urlParse.query.categID}&subcategID=${urlParse.query.subcategID}&productID=1">Product 1</a></li>
+                            <li><a href="/categs/subcategs/products?categID=${urlParse.query.categID}&subcategID=${urlParse.query.subcategID}&productID=2">Product 2</a></li>
+                            <li><a href="/categs/subcategs/products?categID=${urlParse.query.categID}&subcategID=${urlParse.query.subcategID}&productID=3">Product 3</a></li>
+                            <li><a href="/categs/subcategs/products?categID=${urlParse.query.categID}&subcategID=${urlParse.query.subcategID}&productID=4">Product 4</a></li>
+                        </ul>
+                        `
+                    }
+                    
+                }
+                else//localhost:3000/categs/subcategs?categID=1
+                {
+                    statusCode = 200
+                    contentRes = `<h1>Vous êtes sur les catégories secondaires</h1>
+                    <p>Categ principale : ${urlParse.query.categID}</p>
+                    <ul>
+                        <li><a href="/categs/subcategs/products?categID=${urlParse.query.categID}&subcategID=1">Subcateg 1</a></li>
+                        <li><a href="/categs/subcategs/products?categID=${urlParse.query.categID}&subcategID=2">Subcateg 2</a></li>
+                        <li><a href="/categs/subcategs/products?categID=${urlParse.query.categID}&subcategID=3">Subcateg 3</a></li>
+                        <li><a href="/categs/subcategs/products?categID=${urlParse.query.categID}&subcategID=4">Subcateg 4</a></li>
+                    </ul>
+                    `
+                }
+            }
+            else //localhost:3000/categs
+            {
+                statusCode = 200
+                contentRes = `<h1>Vous êtes sur les catégories principales</h1>
+                <ul>
+                    <li><a href="/categs/subcategs?categID=1">Categ principale 1</a></li>
+                    <li><a href="/categs/subcategs?categID=2">Categ principale 2</a></li>
+                    <li><a href="/categs/subcategs?categID=3">Categ principale 3</a></li>
+                    <li><a href="/categs/subcategs?categID=4">Categ principale 4</a></li>
+                </ul>`
+                
+            }
+        }
+        else
+        {
+            statusCode = 404
+            contentRes = "<h1>Oops, page non trouvée</h1>"
+        }
+
+    }
+    else if(req.method == "POST")
+    {
+        if(urlParse.pathname == "/contact")
+        {
+            statusCode = 200
+            contentRes = `<h1>je suis le contact en mode POST</h1>`
+        }
+    }
+    else
+    {
+        statusCode = 404
+        contentRes = `<h1>Je ne connais pas cette méthode HTTP : ${req.method}</h1>`
+    }
+
+
+
+    //localhost:3000/categ/subcateg/product?categID=1&subcategID=2&productID=3
+
+    res.writeHead(statusCode, { "Content-Type" : "text/html; charset=utf-8" })
+    res.write(contentRes)
+    res.end()
+})
+
+server.listen(3000)
